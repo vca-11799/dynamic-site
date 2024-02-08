70eabf02768ac7e3bffdefe564111bd36bec55ad
vca-11799 <vishalagravat555@gmail.com>
2024-02-08 17:58:00 +0530

    bug

--- app/views.py
+++ app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
-from .models import SubProducts
+from django.shortcuts import render, get_object_or_404
+from .models import SubProducts,ProductDetails
 
 # Create your views here.
+def my_view(request):
+        sub_products = SubProducts.objects.all()
+        return {'SubProducts': sub_products}
+
 def home(request):
     return render(request, "index.html")
 
@@ -11,16 +15,11 @@ def about(request):
 def contact(request):
     return render(request, "contact.html")
 
-def product(request):
-    return render(request, "product.html")
+def product(request, pk):
+    sub_product = get_object_or_404(SubProducts, pk=pk)
+    products = ProductDetails.objects.filter(category=sub_product)
+    return render(request, "product.html", {'sub_product': sub_product, 'products': products})
 
-def my_view(request):
-        sub_products = SubProducts.objects.all()
-        return {'SubProducts': sub_products}
-
-
-
-    
 
 
 
